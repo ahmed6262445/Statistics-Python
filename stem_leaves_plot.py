a176ffb84cc1f8bc330bf9a25a2c575226d5d4b5
Ahmed <01-131172-004@student.bahria.edu.pk>
2020-03-19 17:07:57 +0500

    Coverted to function

--- stem_leaves_plot.py
+++ stem_leaves_plot.py
@@ -1,15 +1,24 @@
 from collections import defaultdict
-my_list = [112, 86, 84, 99, 108, 102, 101, 95, 102, 87, 106, 123, 105, 107, 91, 124, 103, 108, 94, 110, 142, 87, 89, 99, 129, 94, 115, 88, 117, 100, 95, 95, 107, 103, 76, 97, 79, 117, 108, 107, 69, 105, 91, 88, 106, 111, 92, 108, 108, 86, 118, 94, 109, 92, 111, 106, 100, 86, 100, 100, 97]
-my_dict = defaultdict(list)
+# my_list = [112, 86, 84, 99, 108, 102, 101, 95, 102, 87, 106, 123, 105, 107, 91, 124, 103, 108, 94, 110, 142, 87, 89, 99, 129, 94, 115, 88, 117, 100, 95, 95, 107, 103, 76, 97, 79, 117, 108, 107, 69, 105, 91, 88, 106, 111, 92, 108, 108, 86, 118, 94, 109, 92, 111, 106, 100, 86, 100, 100, 97]
 
-for  i in range( len(my_list) ):
-    key = int(my_list[i]/10)
-    value = my_list[i]%10
+def make_stem_leaves_plot(number_list):
+    """
+        Arguments:
+        number_list (df or list)
 
-    my_dict[key].append(value)    
-    my_dict[key].sort()
+        Return:
+            Returns stem and leaves plot as dictionary
+    """
+    dict_list = defaultdict(list)
 
-sorted(my_dict.keys()) 
+    for i in range(len(number_list)):
+        key = int(number_list[i]/10)
+        value = number_list[i]%10
+        dict_list[key].append(value)    
+        dict_list[key].sort()
 
-for key,value in sorted(my_dict.items()):
-    print(f"{key}:\t{value} \ttotal :{len(value)}") 
+    new_dict = defaultdict(list)
+
+    for key,value in sorted(dict_list.items()):
+        new_dict[key] = value
+    return new_dict
